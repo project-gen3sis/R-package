437773aca8fc2df8d2de853ef0ec6411d2219a48
ohagen <oskar.hagen@usys.ethz.ch>
2020-04-09 14:18:19 +0200

    EXPERIMENT_X now CaseStudy1

--- inst/examples/run_simulation_help.R
+++ inst/examples/run_simulation_help.R
@@ -2,7 +2,7 @@
 library(gen3sis)
 
 #get correct path or correct input objects
-datapath <- system.file(file.path("extdata", "EXPERIMENT_X"), package="gen3sis")
+datapath <- system.file(file.path("extdata", "CaseStudy1"), package="gen3sis")
 
 #run simulation
 run_simulation(config = file.path(datapath,"config/config.R"), 
