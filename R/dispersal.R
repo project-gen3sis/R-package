7ddc6c2a953bb03fac0fcc25a2ba102bf51a19a6
loic-pellissier <62331405+loic-pellissier@users.noreply.github.com>
2020-04-14 15:05:25 +0200

    Update dispersal.R

--- R/dispersal.R
+++ R/dispersal.R
@@ -1,23 +1,30 @@
 # Copyright (c) 2020, ETH Zurich
 
-#' get_dispersal_values allows the user to define a function that generates dispersal value(s) for a given species.
+#' get_dispersal_values allows the user to generates dispersal value(s) for a given species. The simulation request the user 
+#' to return a vector of dispersal values with length num_draws.
 #'
 #' @details Dispersal values are used for two different operations. First, they are used to evaluate pairwise dispersal
 #' events between colonized and uninhabited cells. Second, they are used during the geographical clustering of
-#' species populations when determining which cells are in range of each other and belong to the same cluster
-#' Note: if the distances are randomized the cluster-formation may be asymetrical. Therefore the ordering of all
+#' species populations when determining which cells are in range of each other and belong to the same geographic cluster.
+#' 
+#' num_draws tells the user how many dispersal values are requested by the simulation when this function is called and must 
+#' be returned. It can be of varying length depending on the operation calling it, i.e. colonisation or geographic clustering. 
+#' If the dispersal is considered as fixed the function should return a vector of length num_draws with repeated identical
+#' values, or varying values in case of more complex dispersal kernels.
+#' 
+#' Note: if the distances are randomized the cluster formation may be asymetrical. Therefore the ordering of all
 #' clustering operations is randomized.
+#' 
 #' @param num_draws 'num_draws' the number of dispersal values drawn
 #' @param species 'species' the species for which the values are to be produced
-#' @param landscape 'landscape' the current landscape
-#' @param config 'config' the current config
+#' @param landscape 'landscape' the landscape of the current time step
+#' @param config 'config' the config of the simulation
 #'
 #' @return a numerical vector of length num_draws with dispersal values
 #' @export
 get_dispersal_values <- function(num_draws, species, landscape, config) {
   stop("this function documents the user function interface only, do not use it")
 }
-
 
 #' The top level loop for dispersal
 #'
@@ -49,10 +56,10 @@ loop_dispersal <- function(config, data, vars){
 #' @details This function selects all suitable cells from the landscape and checks whether the given
 #' species can reach and colonize any one of them.
 #'
-#' @param species The species to disperse
-#' @param landscape The current landscape
-#' @param distance_matrix The distance matrix to check the dispersion capabilities against
-#' @param config the general config object, for the get_dispersal_values function, amongst others
+#' @param species The species to apply dispersal to
+#' @param landscape The landscape of the current time step
+#' @param distance_matrix The distance matrix between cells to check the dispersal capabilities against
+#' @param config the config of the simulation
 #'
 #' @return The dispersed species
 #' @noRd
