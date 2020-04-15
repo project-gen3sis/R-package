74890b5a0133e227acdd393ac23ca36eb8b983c7
loic-pellissier <62331405+loic-pellissier@users.noreply.github.com>
2020-04-15 08:13:00 +0200

    Update gen3sis_landscape.R

--- R/gen3sis_landscape.R
+++ R/gen3sis_landscape.R
@@ -1,17 +1,18 @@
 # Copyright (c) 2020, ETH Zurich
 
-#' create_landscape creates an object representing the current landscape for a given timestep
+#' The function create_landscape creates an object representing the current landscape for a given time step
 #'
-#' @details This is internal representation for a given landscape. Int consists of an id for the current timestep,
-#' an environmental matrix containing the environmental conditions for each suitable cell,
-#' and a coordinates matrix containing the cell-centre coordinates of all suitable cells
+#' @details This function generates the internal representation of a given landscape. It 
+#' consists of an identifier for the input time step, an environmental matrix containing the environmental 
+#' conditions for each suitable cell, and a coordinates matrix containing the cell centre coordinates of 
+#' all suitable cells
 #'
-#' @param id the current timestep ti
-#' @param timestep the current timestep name
+#' @param id the identifier of the input time step
+#' @param timestep the name of the input time step 
 #' @param environment a matrix containing the environmental conditions
-#' @param coordinates a matrix containg the cell-centre coordinates
-#' @param extent the extent of the overall landscape
-#' @param resolution the resolution of the input in coords/enironment
+#' @param coordinates a matrix containg the coordinates of the cells centre
+#' @param extent the extent of the landscape
+#' @param resolution the spatial resolution of the input 
 #'
 #' @return returns a landscape of class "gen3sis_landscape"
 #' @noRd
