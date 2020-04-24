91db0b8b4ed9c604c63e77e23d6e05205e4bffc0
ohagen <oskar.hagen@usys.ethz.ch>
2020-04-24 13:25:06 +0200

    summary plot

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -61,23 +61,85 @@ plot_summary <- function(output) {
   if (class(output)!="gen3sis_output"){
     stop("this is not  a gen3sis_output object")
   }
-  # plotting of end of simulation goes here, like plotting the phylo_summary.
-  grDevices::pdf(file=file.path(config$directories$output, "/EndConditions.pdf"), width=10, height=12)
-  par(mfrow=c(2,1))
-  plot_richness(data$all_species, data$landscape)
   
-  categories <- c("total", "alive", "speciation", "extinctions")
-  colours <- c("black", "magenta", "blue", "orange")
-  matplot(data$summaries$phylo_summary[-1,], 
-          type = "b", 
+  {
+  layout( matrix(c(1,3,3,1,3,3,2,2,2,2,2,2),ncol=3, byrow =T)  )
+  layout.show(3)
+  par(mar=c(7.3,3,0,7.5), oma=c(0.3,0.8,0.3,0.8))
+  
+  # summary text
+  plot(1, type="n", xlab="", ylab="", xlim=c(0, 10), ylim=c(0, 10),axes=FALSE,ann=FALSE)
+  #title
+  text(1,10, paste("Simulation", ), font=2)
+  
+  # time behaviour
+  d <- output$summary$phylo_summary[-1,-1]
+  plot( d[,"alive"],  xlab="", ylab="", type='l', col="black", lwd=4, frame.plot = FALSE, xaxt='n', yaxt='n')
+  
+  
+  axis(4,line=-1, cex=1, cex.axis=1, col="black")
+  mtext(side = 4, text = "Species richness", col = "black", line = 2, cex=1.1)
+  
+  par(new=TRUE)
+  plot( d[,"speciations"],  pch=3, col=rgb(0,0,1, 0.5), xlab="", ylab="", type='b',frame.plot = FALSE, xaxt='n', yaxt='n', ylim=range(d[,c("speciations", "extinctions")]))
+  points(d[,"extinctions"], pch=4, col=rgb(1,0,0, 0.5), type="b")
+  
+  axis(2,line=-1, cex=1, cex.axis=1, col="black")
+  mtext(side = 2, text = "Evolutionary events", col = "black", line = 1.5, cex=1.1)
+  
+  legend(x=1, y=max(d[,c("speciations", "extinctions")]), legend=c("Speciation", "Extinction"), col=c(rgb(0,0,1, 0.5), rgb(1,0,0, 0.5)), pch=c(3,4),  bty = "n")
+  
+  axis(1)
+  mtext(side=1, text="Time steps", line=2.5, cex=1.1)
+  
+  
+  # richness map
+  col_vec <- colorRampPalette(c( "snow2", "yellow", "orange" ,"red", "darkred", "chocolate4")  )(max(output$summary$`richness-final`[,3], na.rm=T))
+  
+  image(rasterFromXYZ(output$summary$`richness-final`), col=col_vec, bty = "n", xlab = "", ylab = "", main="bla")
+  title("Species richness map at final step", line=-1)
+  plot(rasterFromXYZ(output$summary$`richness-final`), legend.only=T, add=T,col=col_vec)
+  
+  }
+  
+
+  
+  
+       #      , lwd=3, frame.plot = FALSE, xaxt='n', yaxt='n')
+  
+  # b 
+  # layout( matrix(c(1,2,2,1,2,2,3,3,3,3,3,3),ncol=3, byrow =T)  )
+  # layout.show(3)
+  # par(mar=c(7.3,3,0,7.5), oma=c(0.3,0.8,0.3,0.8))
+  # # TEMP
+  # # Base plot!
+  # plot(mean_vars[,c("Ma", "Mean_Temperature")], xlim=rev(range(mean_vars[, "Ma"], na.rm=T)),  xlab="", ylab="", ylim=c(range_mean_temp_plot[1], range_mean_temp_plot[2]), type='l', col="red" 
+  #      , lwd=3, frame.plot = FALSE, xaxt='n', yaxt='n')
+  # at = axTicks(2)
+  # axis(2,labels = FALSE, line=-2.5, cex=2, cex.axis=3, col="red")
+  # mtext(side = 2, text = at, at = at, col = "red", line = -1, cex=1.5)
+  # points(mean_vars[which(round(mean_vars[,"Ma"],2)==as.numeric(formatedyear)), "Ma"], 
+  #        mean_vars[which(round(mean_vars[,"Ma"],2)==as.numeric(formatedyear)),"Mean_Temperature"]
+  # 
+  
+  
+  
+  
+  categories <- c("alive", "speciation", "extinctions")
+  colours <- c("black", "blue", "red")
+  matplot(output$summary$phylo_summary[-1,-1], line = 0.5,
+          type = "o", 
           lty = 1, 
-          pch = 1:4,
+          pch = 2:4,
           col = colours,
-          xlab = "timesteps",
-          ylab = "# of events",
+          xlab = "timestep",
+          ylab = "Eevents",
           main = "Species development through time")
   
   legend("topright", col=colours, categories, bg="white", lwd=1, pch=1:4)
+  
+  # c
+  
   
   grDevices::dev.off()
 }
