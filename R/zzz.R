5acdbc41f7ec489406cb699258fe6b82163fc905
ohagen <oskar.hagen@usys.ethz.ch>
2020-04-29 18:21:49 +0200

    comment lines

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 # Copyright (c) 2020, ETH Zurich
 
-.onLoad <- function(...) {
-  packageStartupMessage("Gen3sis: General Engine for Eco-Evolutionary Simulations. \n  Copyright (c) 2020, ETH Zurich  \n This program comes with ABSOLUTELY NO WARRANTY; This is free software, and you are welcome to redistribute it under certain conditions. \n \n \n **********THIS IS A BETA VERSION ... lets call it 0.9.9 getting there people**********")
-}
+# .onLoad <- function(...) {
+#   packageStartupMessage("Gen3sis: General Engine for Eco-Evolutionary Simulations. \n  Copyright (c) 2020, ETH Zurich  \n This program comes with ABSOLUTELY NO WARRANTY; This is free software, and you are welcome to redistribute it under certain conditions. \n")
+# }
