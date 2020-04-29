d657d3d6bcac7a95282832ed963aa22bfeb020ce
ohagen <oskar.hagen@usys.ethz.ch>
2020-04-29 18:12:10 +0200

    beta release internal

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 # Copyright (c) 2020, ETH Zurich
 
-#check for best practices on inport print messages
-# .onLoad <- function(...) {
-#   packageStartupMessage("Gen3sis: General Engine for Eco-Evolutionary Simulations. \n  Copyright (c) 2020, ETH Zurich [Oskar Hagen, Benjamin Flueck and Loic Pellissier] \n This program comes with ABSOLUTELY NO WARRANTY; This is free software, and you are welcome to redistribute it under certain conditions. \n ")
-# }
+check for best practices on inport print messages
+.onLoad <- function(...) {
+  packageStartupMessage("Gen3sis: General Engine for Eco-Evolutionary Simulations. \n  Copyright (c) 2020, ETH Zurich  \n This program comes with ABSOLUTELY NO WARRANTY; This is free software, and you are welcome to redistribute it under certain conditions. \n \n \n **********THIS IS A BETA VERSION ... lets call it 0.9.9 getting there people**********")
+}
