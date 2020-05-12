6673ab242a2d4343a9bf524bc65a699215ae6677
ohagen <oskar.hagen@usys.ethz.ch>
2020-05-12 21:20:57 +0200

    changed default call observer to all and enhanced help of run_simulation

--- R/gen3sis_main.R
+++ R/gen3sis_main.R
@@ -22,8 +22,8 @@ assign("dist", -Inf, envir = counting)
 #' @param config configuration file for the simulation or configuration object derived from a config file
 #' @param landscape directory where the all_geo_hab and distance_matrices reside
 #' @param output_directory directory for the simulation output
-#' @param timestep_restart = Set the start time timestep. If NA start at the beginning, If "ti" start from the last available timestep, if a number "x" start from timestep x
-#' @param save_state = Save the internal state of the simulation for restarts, if "all" save all timestep, if a vector, saves the desired timesteps if "last", saves only last timestep
+#' @param timestep_restart = set the start time timestep. If NA start at the beginning, If "ti" start from the last available timestep, if a number "x" start from timestep x
+#' @param save_state = save the internal state of the simulation for restarts. If "all" save all timestep, if a vector, saves the desired timesteps if "last", saves only last timestep
 #' @param call_observer call observer functions if any, NA calls at the start and end times, "all" call all timesteps or provide the timesteps to be stored 
 #' @param enable_gc enable gc in case of memory shortages
 #' @param verbose integer value (0, 1 ,2 or 3). If 0 no printed statement, 1 timestep progress, 2 enable additional progress outputs regarding current timestep, 3 aditional information from within modules (default is 1)
@@ -40,7 +40,7 @@ run_simulation <- function(config = NA,
                           output_directory = NA, 
                           timestep_restart = NA,
                           save_state = NA,
-                          call_observer = NA,
+                          call_observer = "all",
                           enable_gc = F,
                           verbose = 1){
   
