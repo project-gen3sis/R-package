79d0beea5e0548b0387ae7d627346095bd37c6ba
Oskar Hagen <oskar.hagen@outlook.com>
2020-05-18 13:21:02 +0200

    fix #20 and created a test
    for the run_simulation with milinal 5 timesptes and location at tempdir()

--- tests/testthat/test-gen3sis_main.R
+++ tests/testthat/test-gen3sis_main.R
@@ -3,6 +3,15 @@
 
 test_that("run_simulation works", {
   skip_on_cran()
-  s <- run_simulation(....elt())
-  expect_true(!isnull(s))
+  #get correct path or correct input objects
+  #take data from case study 1
+  datapath <- system.file(file.path("extdata", "CaseStudy1"), package="gen3sis")
+  config <- create_config(file.path(datapath,"config/config_rte.R"))
+  #run only the latest timesteps
+  config$gen3sis$general$start_time <- 5
+  #re-set call of observer
+  config$gen3sis$general$end_of_timestep_observer <- function(data, vars, config){}
+  s <- run_simulation(config = config, 
+                      landscape = file.path(datapath,"landscape"), output_directory = tempdir())
+  expect_true(!is.null(s))
 })
