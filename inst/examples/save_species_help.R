874945c930fa6d70d5bf39f012cc0a0afb32a437
Oskar Hagen <oskar.hagen@outlook.com>
2020-06-19 14:20:29 +0200

    add save_species example

--- inst/examples/save_species_help.R
+++ inst/examples/save_species_help.R
@@ -1,1 +1,9 @@
-##TODO##
+\dontrun{
+#adding the call to the end_of_timestep_observer function at the config file or object 
+#will automatically save all the species at an rds file at the outputfolder/species folder
+# and the respective landscape at outputfolder/landscapes for the times steps the observer 
+# function is called (i.e. call_observer parameter at the run_simulation function)
+save_species()
+  
+  
+}
