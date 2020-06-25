76ed2a054455c930036520547054fd7bdf17058f
Oskar Hagen <oskar.hagen@outlook.com>
2020-06-26 01:15:34 +0200

    details for readability example plot_species_presence help

--- inst/examples/plot_species_presence_help.R
+++ inst/examples/plot_species_presence_help.R
@@ -3,16 +3,16 @@ datapath <- system.file(file.path("extdata", "WorldCenter"), package = "gen3sis"
 
 # load landscape and species at time step zero
 landscape_t_0 <- readRDS(
-  file.path(datapath, "output", "config_worldcenter", "landscapes", "landscape_t_0.rds"))
+  file.path(datapath, "output/config_worldcenter", "landscapes", "landscape_t_0.rds"))
 species_t_0 <- readRDS(
-  file.path(datapath, "output", "config_worldcenter", "species", "species_t_0.rds"))
+  file.path(datapath, "output/config_worldcenter", "species", "species_t_0.rds"))
 
-## plot species 21 range
+# plot species 21 range
 plot_species_presence(species_t_0[[21]], landscape_t_0)
 # oh, a South American one!
 
-## plot occurrence of 3 species
+# plot ranges of 3 species (i.e. 1, 21 and 32)
 par(mfrow=c(1,3))
 plot_species_presence(species_t_0[[1]], landscape_t_0)
 plot_species_presence(species_t_0[[21]], landscape_t_0)
-plot_species_presence(species_t_0[[31]], landscape_t_0)
+plot_species_presence(species_t_0[[32]], landscape_t_0)
