437d1a82ded9263db9bae03aa0d3313b15d6262f
Oskar Hagen <oskar.hagen@outlook.com>
2020-06-30 19:43:04 +0200

    add documentation

--- inst/examples/run_simulation_help.R
+++ inst/examples/run_simulation_help.R
@@ -1,10 +1,8 @@
-## running a simple simulation
-
+\dontrun{
 # get path or correct input objects
 datapath <- system.file(file.path("extdata", "CaseStudy1"), package="gen3sis")
 
 # run simulation and store summary obejct to sim
-\dontrun{
 sim <- run_simulation(config = file.path(datapath,"config/config_fast.R"), 
                 landscape = file.path(datapath,"landscape"))
 
