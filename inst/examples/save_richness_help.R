22faf17c761a5c10a3415186610ac29e3df68733
Oskar Hagen <oskar.hagen@outlook.com>
2020-07-01 08:45:45 +0200

    Update save_richness_help.R

--- inst/examples/save_richness_help.R
+++ inst/examples/save_richness_help.R
@@ -1,1 +1,5 @@
-##TODO##
+\dontrun{
+  ## save the current richness pattern from within observer for each species
+  # this functions should be called inside the end_of_timestep_observer function at the config file:
+  save_richness()
+}
