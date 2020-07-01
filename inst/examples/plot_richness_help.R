a21dc46814eb661ac1c9e62ab255e6562b135556
Oskar Hagen <oskar.hagen@outlook.com>
2020-07-01 09:28:25 +0200

    shorten example lines

--- inst/examples/plot_richness_help.R
+++ inst/examples/plot_richness_help.R
@@ -4,14 +4,17 @@
   # get path containing example rasters
   datapath <- system.file(file.path("extdata", "WorldCenter"), package="gen3sis")
   # get species at t0
-  species_t_0 <- readRDS(file.path(datapath, "output/config_worldcenter/species/species_t_0.rds"))
+  species_t_0 <- readRDS(file.path(datapath, 
+                          "output/config_worldcenter/species/species_t_0.rds"))
   # get landscape at t0
-  landscape_t_0 <- readRDS(file.path(datapath, "output/config_worldcenter/landscapes/landscape_t_0.rds"))
+  landscape_t_0 <- readRDS(file.path(datapath, 
+                            "output/config_worldcenter/landscapes/landscape_t_0.rds"))
   # plot richness
   plot_richness(species_t_0, landscape_t_0)
   
   
   ## plot from within observer
-  ## call plot_richness from inside the end_of_timestep_observer function at the config file:
+  # call plot_richness from inside the end_of_timestep_observer function 
+  # at the config file:
   plot_richness(data$all_species, data$landscape)
 }  
