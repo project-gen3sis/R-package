97863aa3aa3d598d04b404d1deb321b77bcb9d2c
Oskar Hagen <oskar.hagen@outlook.com>
2020-07-01 08:45:01 +0200

    Update save_phylogeny_help.R

--- inst/examples/save_phylogeny_help.R
+++ inst/examples/save_phylogeny_help.R
@@ -1,1 +1,5 @@
-##TODO##
+\dontrun{
+  ## save phylogeny as a nexus tree from within observer for each species
+  # this functions should be called inside the end_of_timestep_observer function at the config file:
+  save_phylogeny()
+}
