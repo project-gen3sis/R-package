68f34e9e8346b5e0f361fd7118d5c2cf095fb5b7
Oskar Hagen <oskar.hagen@outlook.com>
2020-07-01 09:27:47 +0200

    shorten example lines

--- inst/examples/get_geo_richness_help.R
+++ inst/examples/get_geo_richness_help.R
@@ -2,9 +2,11 @@
   # get path containing example rasters
   datapath <- system.file(file.path("extdata", "WorldCenter"), package="gen3sis")
   # get species at t0
-  species_t_0 <- readRDS(file.path(datapath, "output/config_worldcenter/species/species_t_0.rds"))
+  species_t_0 <- readRDS(file.path(datapath, 
+                          "output/config_worldcenter/species/species_t_0.rds"))
   # get landscape at t0
-  landscape_t_0 <- readRDS(file.path(datapath, "output/config_worldcenter/landscapes/landscape_t_0.rds"))
+  landscape_t_0 <- readRDS(file.path(datapath, 
+                            "output/config_worldcenter/landscapes/landscape_t_0.rds"))
   # get geo richness
   richness_t_0 <- get_geo_richness(species_t_0, landscape_t_0)
   
