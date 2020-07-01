64d3c83a22a5a3c4be95267a2a807b3217519bdf
Oskar Hagen <oskar.hagen@outlook.com>
2020-07-01 21:42:48 +0200

    clean up and typo-fixes for CRAN submission

--- inst/examples/write_config_skeleton_help.R
+++ inst/examples/write_config_skeleton_help.R
@@ -1,6 +1,6 @@
 \dontrun{
 # set config_empty.R file path
-config_file_path <- file.path("./FOLDER", "EmptyConfig", "config_empty.R")
+config_file_path <- file.path("YOUR_OUTPUT_DIRECTORY_HERE", "config_empty.R")
 #writes out a config skeleton
 write_config_skeleton(config_file_path)
 }
