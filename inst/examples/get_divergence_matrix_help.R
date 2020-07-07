73930532d39e117191c8d4f245026df97b56d757
Oskar Hagen <oskar.hagen@outlook.com>
2020-07-07 10:42:52 +0200

    update documents to fix build and remove dontrun from 2 examples

--- inst/examples/get_divergence_matrix_help.R
+++ inst/examples/get_divergence_matrix_help.R
@@ -1,4 +1,3 @@
-\dontrun{
 # get path containing example rasters
 datapath <- system.file(file.path("extdata", "WorldCenter"), package="gen3sis")
 # get species at t0
@@ -8,4 +7,3 @@ divergence_sp1_t0 <- get_divergence_matrix(species_t_0[[1]])
 # get divergence matrix from species 12
 divergence_sp12_t0 <- get_divergence_matrix(species_t_0[[12]])
 # note that species 1 has no divergence between it's populations, while 12 has.
-}
