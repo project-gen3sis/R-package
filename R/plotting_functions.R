d9c4ed9d010b0d04a408b3341ddab41b954d2c15
Oskar Hagen <oskar.hagen@outlook.com>
2020-07-07 10:18:36 +0200

    Update plotting_functions.R

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -81,7 +81,7 @@ plot_summary <- function(output, summary_title=NULL, summary_legend=NULL) {
                    1,3,3,
                    1,3,3,
                    2,2,2,
-                   2,2,2),ncol=3, byrow =T)  )
+                   2,2,2),ncol=3, byrow =TRUE)  )
   #layout.show(3)
   par(mar=c(4,3,3,7), oma=c(0.1,0.8,0.3,0.8))
   
