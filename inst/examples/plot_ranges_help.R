120dce6c405c57c45e0fc81fe79c2501566557f8
Oskar Hagen <oskar.hagen@outlook.com>
2020-11-09 22:00:14 +0100

    Update plot_ranges_help.R

--- inst/examples/plot_ranges_help.R
+++ inst/examples/plot_ranges_help.R
@@ -1,30 +1,23 @@
 ## plot from saved outputs
-# get path containing example rasters
+# get path containing outputs
 datapath <- system.file(file.path("extdata", "WorldCenter"), package="gen3sis")
 # get species at t0
-species_t_300 <- readRDS(file.path(datapath, 
+species_t_300 <- readRDS(file.path(datapath,
                         "output/config_worldcenter/species/species_t_300.rds"))
 # get landscape at t0
-landscape_t_300 <- readRDS(file.path(datapath, 
+landscape_t_300 <- readRDS(file.path(datapath,
                           "output/config_worldcenter/landscapes/landscape_t_300.rds"))
 # plot range
 plot_ranges(species_t_300, landscape_t_300)
 
-
 # get species at t0
 species_t_150 <- readRDS(file.path(datapath, 
-                                   "output/config_worldcenter/species/species_t_150.rds"))
+                        "output/config_worldcenter/species/species_t_150.rds"))
 # get landscape at t0
 landscape_t_150 <- readRDS(file.path(datapath, 
-                                     "output/config_worldcenter/landscapes/landscape_t_150.rds"))
-
+                        "output/config_worldcenter/landscapes/landscape_t_150.rds"))
 # plot ranges at intermediate time-step
-plot_ranges(species_t_150, landscape_t_150, disturb = 2, max_sps = 5)
-
-
-
-
-
+plot_ranges(species_t_150, landscape_t_150, disturb = 2, max_sps = 20)
 
 ## plot from within observer
 # call plot_richness from inside the end_of_timestep_observer function 
