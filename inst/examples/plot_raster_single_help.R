b3e2a4d4d7b312ea8a3b277fd9b51f570facc9de
Oskar Hagen <oskar.hagen@outlook.com>
2020-12-15 12:46:35 +0100

    Update plot_raster_single_help.R

--- inst/examples/plot_raster_single_help.R
+++ inst/examples/plot_raster_single_help.R
@@ -6,9 +6,9 @@ landscape_t_25 <- readRDS(
   file.path(datapath, "output", "config_worldcenter", "landscapes", "landscape_t_25.rds"))
 oldpar <- par(no.readonly = TRUE)
 par(mfrow=c(1,2))
-plot_raster_single(landscape_t_150$environment[,"temp"], landscape_t_150, "Temperature", NA)
+plot_raster_single(landscape_t_25$environment[,"temp"], landscape_t_25, "Temperature", NA)
 # use col to change the color
-plot_raster_single(landscape_t_150$environment[,"arid"], landscape_t_150, "Aridity", NA, 
+plot_raster_single(landscape_t_25$environment[,"arid"], landscape_t_25, "Aridity", NA, 
                    col=topo.colors(5))
 par(oldpar)
 # note that these values were scaled by the configuration object
