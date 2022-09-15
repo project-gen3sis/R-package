22b5a6b04c1fadb9e73b9ae93a179280192bbddb
Benjamin Flück <benj@gmx.net>
2022-09-15 16:59:09 +0200

    update documentation and autogenerated files

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // getEntities
 IntegerVector getEntities(NumericMatrix ma);
 RcppExport SEXP _gen3sis_getEntities(SEXP maSEXP) {
